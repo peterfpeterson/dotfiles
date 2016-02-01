9a4f7bb8bc9e0d34370cfdf989542f434a723fd5
Pete Peterson <peterfpeterson@gmail.com>
2016-02-01 10:58:58 -0500

    Moved to using wari

---
+++ wari-install.sh
@@ -0,0 +1,16 @@
+export WARI_ROOT=`pwd`/wari
+
+
+. $WARI_ROOT/wari-setup.sh
+
+add_repo chrome
+add_repo devtoolset-3
+add_repo elrepo epel
+add_repo mantid
+add_repo sns
+
+add_packages_from_distro_file
+
+#add_package morebin mantid-developer
+
+echo "Done"
