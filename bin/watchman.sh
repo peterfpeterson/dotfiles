25f602b934d03b822c122d286e4ecfa26f5fea05
Pete Peterson <petersonpf@ornl.gov>
2017-02-02 15:16:10 -0500

    Cleanup code

--- bin/watchman.sh
+++ bin/watchman.sh
@@ -10,37 +10,65 @@ help () {
     echo "start  Delete the triggers and shutdown the watchman server"
     echo "stop   Delete the triggers and shutdown the watchman server"
     echo "status List the triggers in this directory"
+    echo ""
+    echo "Example configuration file:"
+    echo " [\"trigger\", \"/home/username/hotscience\","
+    echo " {"
+    echo "     \"name\": \"latex\","
+    echo "     \"append_files\": true,"
+    echo "     \"expression\": ["
+    echo "         \"anyof\","
+    echo "         [\"match\", \"*.tex\", \"wholename\"]"
+    echo "     ],"
+    echo "     \"command\": [\"pdflatex\", \"awesome_paper.tex\"]"
+    echo "    }"
+    echo " ]"
+
 }
 
+########## check for dependencies
 if [ ! $(command -v watchman) ]; then
     echo "failed to find watchman https://facebook.github.io/watchman/"
     exit -1
 fi
-
-if [ ! $(command -v watchman) ]; then
+if [ ! $(command -v jq) ]; then
     echo "failed to find jq https://stedolan.github.io/jq/"
     exit -1
 fi
 
+########## exit early on help
 if [ $# == 0 ]; then
     help
     exit -1
 fi
+if [ $1 == "help" ]; then
+    help
+    exit
+fi
 
+########## determine configuration file
 if [ $# == 2 ]; then
     CONFIGFILE="$2"
 else
     CONFIGFILE=watchman.json
 fi
+if [ ! -f ${CONFIGFILE} ]; then
+    echo "error: Could not open file ${CONFIGFILE}: No such file or directory"
+    exit -1
+fi
 
+########## switch to the specified directory
 DIR=$(jq -r -M .[1] ${CONFIGFILE})
+if [ ! -d ${DIR} ]; then
+    echo "error: invalid directory ${DIR}"
+    exit -1
+fi
 cd ${DIR}
+DIR=$(pwd)
 
+########## do the actual work
 case "$1" in
-    help)
-        help
-        exit
-        ;;
+    # "help" is dealt with above
     start)
         # startup watchman
         echo "start watching ${DIR}"
@@ -58,11 +86,8 @@ case "$1" in
         ;;
     *)
         echo "unknown command \"$1\""
-        cd -
         exit -1
         ;;
 esac
-
-cd -
 
 exit
