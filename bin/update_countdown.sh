fc364f281e95324ddd1dcc1bcd9f36dd685a476f
Pete Peterson <petersonpf@ornl.gov>
2017-04-07 15:50:55 -0400

    Change formating

--- bin/update_countdown.sh
+++ bin/update_countdown.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
-echo "  $(daysuntil 2017-04-08) days til Saw Works" > ~/.daysuntil.deadlines
-echo " $(daysuntil 2017-04-21) days til Atlanta Ragnar" >> ~/.daysuntil.deadlines
-echo " $(daysuntil 2017-05-05) days til dPDF meeting" >> ~/.daysuntil.deadlines
-echo " $(daysuntil 2017-05-08) days til total scattering school" >> ~/.daysuntil.deadlines
-echo " $(daysuntil 2017-05-20) days til beer, bbq, bourbon" >> ~/.daysuntil.deadlines
-echo " $(daysuntil 2017-07-12) days til mantid dev" >> ~/.daysuntil.deadlines
-echo " $(daysuntil 2017-07-12) days til mantid 3.10 release" >> ~/.daysuntil.deadlines
-echo "$(daysuntil 2017-07-17) days til brewfest" >> ~/.daysuntil.deadlines
-echo "$(daysuntil 2017-09-17) days til BRR" >> ~/.daysuntil.deadlines
-echo "$(daysuntil 2017-12-18) days til CSNS review" >> ~/.daysuntil.deadlines
+echo "  $(daysuntil 2017-04-08) days - Saw Works" > ~/.daysuntil.deadlines
+echo " $(daysuntil 2017-04-21) days - Atlanta Ragnar" >> ~/.daysuntil.deadlines
+echo " $(daysuntil 2017-05-05) days - dPDF meeting" >> ~/.daysuntil.deadlines
+echo " $(daysuntil 2017-05-08) days - total scattering school" >> ~/.daysuntil.deadlines
+echo " $(daysuntil 2017-05-20) days - beer, bbq, bourbon" >> ~/.daysuntil.deadlines
+echo " $(daysuntil 2017-07-12) days - mantid dev" >> ~/.daysuntil.deadlines
+echo " $(daysuntil 2017-07-12) days - mantid 3.10 release" >> ~/.daysuntil.deadlines
+echo "$(daysuntil 2017-07-17) days - brewfest" >> ~/.daysuntil.deadlines
+echo "$(daysuntil 2017-09-17) days - BRR" >> ~/.daysuntil.deadlines
+echo "$(daysuntil 2017-12-18) days - CSNS review" >> ~/.daysuntil.deadlines
