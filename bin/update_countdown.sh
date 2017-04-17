b4d4ccd1988c37ecc58dddedd351de0a7e8cbb36
Pete Peterson <petersonpf@ornl.gov>
2017-04-17 13:07:11 -0400

    Crontab has no PATH

--- bin/update_countdown.sh
+++ bin/update_countdown.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
-echo "  $(daysuntil 2017-04-08) days - Saw Works" > ~/.daysuntil.deadlines
-echo " $(daysuntil 2017-04-21) days - Atlanta Ragnar" >> ~/.daysuntil.deadlines
-echo " $(daysuntil 2017-05-05) days - dPDF meeting" >> ~/.daysuntil.deadlines
-echo " $(daysuntil 2017-05-08) days - total scattering school" >> ~/.daysuntil.deadlines
-echo " $(daysuntil 2017-05-20) days - beer, bbq, bourbon" >> ~/.daysuntil.deadlines
-echo " $(daysuntil 2017-07-12) days - mantid dev" >> ~/.daysuntil.deadlines
-echo " $(daysuntil 2017-07-12) days - mantid 3.10 release" >> ~/.daysuntil.deadlines
-echo "$(daysuntil 2017-07-17) days - brewfest" >> ~/.daysuntil.deadlines
-echo "$(daysuntil 2017-09-17) days - BRR" >> ~/.daysuntil.deadlines
-echo "$(daysuntil 2017-12-18) days - CSNS review" >> ~/.daysuntil.deadlines
+echo "  $(~/bin/daysuntil 2017-04-21) days - Atlanta Ragnar" > ~/.daysuntil.deadlines
+echo " $(~/bin/daysuntil 2017-05-05) days - dPDF meeting" >> ~/.daysuntil.deadlines
+echo " $(~/bin/daysuntil 2017-05-08) days - total scattering school" >> ~/.daysuntil.deadlines
+echo " $(~/bin/daysuntil 2017-05-20) days - beer, bbq, bourbon" >> ~/.daysuntil.deadlines
+echo " $(~/bin/daysuntil 2017-07-12) days - mantid dev" >> ~/.daysuntil.deadlines
+echo " $(~/bin/daysuntil 2017-07-12) days - mantid 3.10 release" >> ~/.daysuntil.deadlines
+echo " $(~/bin/daysuntil 2017-07-17) days - brewfest" >> ~/.daysuntil.deadlines
+echo "$(~/bin/daysuntil 2017-09-08) days - BRR" >> ~/.daysuntil.deadlines
+echo "$(~/bin/daysuntil 2017-09-16) days - Saw Works beer mile" >> ~/.daysuntil.deadlines
+echo "$(~/bin/daysuntil 2017-12-18) days - CSNS review" >> ~/.daysuntil.deadlines
