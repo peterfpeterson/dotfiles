c9f8a13653a55498f166a304724e9b34aaac96b7
Pete Peterson <petersonpf@ornl.gov>
2017-05-23 16:05:07 -0400

    Fixing dates

--- bin/update_countdown.sh
+++ bin/update_countdown.sh
@@ -1,8 +1,7 @@
 #!/bin/sh
-echo "  $(~/bin/daysuntil 2017-05-20) days - beer, bbq, bourbon" > ~/.daysuntil.deadlines
-echo " $(~/bin/daysuntil 2017-07-12) days - mantid dev" >> ~/.daysuntil.deadlines
-echo " $(~/bin/daysuntil 2017-07-12) days - mantid 3.10 release" >> ~/.daysuntil.deadlines
-echo " $(~/bin/daysuntil 2017-07-17) days - brewfest" >> ~/.daysuntil.deadlines
+echo " $(~/bin/daysuntil 2017-06-12) days - mantid dev" > ~/.daysuntil.deadlines
+echo " $(~/bin/daysuntil 2017-06-12) days - mantid 3.10 release" >> ~/.daysuntil.deadlines
+echo " $(~/bin/daysuntil 2017-06-17) days - brewfest" >> ~/.daysuntil.deadlines
 echo "$(~/bin/daysuntil 2017-09-08) days - BRR" >> ~/.daysuntil.deadlines
 echo "$(~/bin/daysuntil 2017-09-16) days - Saw Works beer mile" >> ~/.daysuntil.deadlines
 echo "$(~/bin/daysuntil 2017-12-18) days - CSNS review" >> ~/.daysuntil.deadlines
