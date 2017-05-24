c320f992434d0c867504465422af5c7f388e48fe
Pete Peterson <petersonpf@ornl.gov>
2017-05-24 09:02:36 -0400

    Leftpad in bash

--- bin/update_countdown.sh
+++ bin/update_countdown.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
-echo " $(~/bin/daysuntil 2017-06-12) days - mantid dev" > ~/.daysuntil.deadlines
-echo " $(~/bin/daysuntil 2017-06-12) days - mantid 3.10 release" >> ~/.daysuntil.deadlines
-echo " $(~/bin/daysuntil 2017-06-17) days - brewfest" >> ~/.daysuntil.deadlines
+echo "$(~/bin/daysuntil 2017-06-12) days - mantid dev" > ~/.daysuntil.deadlines
+echo "$(~/bin/daysuntil 2017-06-12) days - mantid 3.10 release" >> ~/.daysuntil.deadlines
+echo "$(~/bin/daysuntil 2017-06-27) days - fedora 26" >> ~/.daysuntil.deadlines
+echo "$(~/bin/daysuntil 2017-06-17) days - brewfest" >> ~/.daysuntil.deadlines
 echo "$(~/bin/daysuntil 2017-09-08) days - BRR" >> ~/.daysuntil.deadlines
 echo "$(~/bin/daysuntil 2017-09-16) days - Saw Works beer mile" >> ~/.daysuntil.deadlines
 echo "$(~/bin/daysuntil 2017-12-18) days - CSNS review" >> ~/.daysuntil.deadlines
