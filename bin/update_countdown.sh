53bbddc93c29d709371863df0172dc096803c450
Pete Peterson <petersonpf@ornl.gov>
2017-06-19 09:14:08 -0400

    Remvoe past events

--- bin/update_countdown.sh
+++ bin/update_countdown.sh
@@ -1,9 +1,6 @@
 #!/bin/sh
-echo "$(~/bin/daysuntil 2017-06-12) days - mantid dev" > ~/.daysuntil.deadlines
-echo "$(~/bin/daysuntil 2017-06-12) days - mantid 3.10 release" >> ~/.daysuntil.deadlines
-echo "$(~/bin/daysuntil 2017-06-17) days - brewfest" >> ~/.daysuntil.deadlines
 # https://fedoraproject.org/wiki/Releases/26/Schedule
-echo "$(~/bin/daysuntil 2017-06-27) days - fedora 26" >> ~/.daysuntil.deadlines
+echo "$(~/bin/daysuntil 2017-07-11) days - fedora 26" > ~/.daysuntil.deadlines
 echo "$(~/bin/daysuntil 2017-09-08) days - BRR" >> ~/.daysuntil.deadlines
 echo "$(~/bin/daysuntil 2017-09-16) days - Saw Works beer mile" >> ~/.daysuntil.deadlines
 echo "$(~/bin/daysuntil 2017-12-18) days - CSNS review" >> ~/.daysuntil.deadlines
