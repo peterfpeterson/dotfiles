e8b0f88a442be0423418243ce982f6eeb003dfa5
Pete Peterson <petersonpf@ornl.gov>
2017-07-13 09:47:10 -0400

    f26 has been released!

--- bin/update_countdown.sh
+++ bin/update_countdown.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 # https://fedoraproject.org/wiki/Releases/26/Schedule
-echo "$(~/bin/daysuntil 2017-09-08) days - BRR" >> ~/.daysuntil.deadlines
+echo "$(~/bin/daysuntil 2017-09-08) days - BRR" > ~/.daysuntil.deadlines
 echo "$(~/bin/daysuntil 2017-09-16) days - Saw Works beer mile" >> ~/.daysuntil.deadlines
 echo "$(~/bin/daysuntil 2017-12-18) days - CSNS review" >> ~/.daysuntil.deadlines
