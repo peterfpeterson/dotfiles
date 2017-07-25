5ed4456b5437b0c8d8cbe1667b7c214d353242c0
Pete Peterson <petersonpf@ornl.gov>
2017-07-25 11:06:56 -0400

    Add Farragut race

--- bin/update_countdown.sh
+++ bin/update_countdown.sh
@@ -2,4 +2,5 @@
 # https://fedoraproject.org/wiki/Releases/26/Schedule
 echo "$(~/bin/daysuntil 2017-09-08) days - BRR" > ~/.daysuntil.deadlines
 echo "$(~/bin/daysuntil 2017-09-16) days - Saw Works beer mile" >> ~/.daysuntil.deadlines
+echo "$(~/bin/daysuntil 2017-10-28) days - Farragut 13.1" >> ~/.daysuntil.deadlines
 echo "$(~/bin/daysuntil 2017-12-18) days - CSNS review" >> ~/.daysuntil.deadlines
