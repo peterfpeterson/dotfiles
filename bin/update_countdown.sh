f78392bb9b761db5869dea1ffb1058bbfacf5b4d
Pete Peterson <petersonpf@ornl.gov>
2017-08-04 09:23:43 -0400

    Add cruise

--- bin/update_countdown.sh
+++ bin/update_countdown.sh
@@ -4,3 +4,4 @@ echo "$(~/bin/daysuntil 2017-09-08) days - BRR" > ~/.daysuntil.deadlines
 echo "$(~/bin/daysuntil 2017-09-16) days - Saw Works beer mile" >> ~/.daysuntil.deadlines
 echo "$(~/bin/daysuntil 2017-10-28) days - Farragut 13.1" >> ~/.daysuntil.deadlines
 echo "$(~/bin/daysuntil 2017-12-18) days - CSNS review" >> ~/.daysuntil.deadlines
+echo "$(~/bin/daysuntil 2018-03-10) days - Cruise" >> ~/.daysuntil.deadlines
