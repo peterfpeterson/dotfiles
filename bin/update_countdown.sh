164f7f3a5d79d6c56fcfa4b3c2fc9ff35a75cf04
Pete Peterson <petersonpf@ornl.gov>
2017-09-11 14:50:37 -0400

    Add python2 death date

--- bin/update_countdown.sh
+++ bin/update_countdown.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # https://fedoraproject.org/wiki/Releases/26/Schedule
-echo "$(~/bin/daysuntil 2017-09-08) days - BRR" > ~/.daysuntil.deadlines
-echo "$(~/bin/daysuntil 2017-09-16) days - Saw Works beer mile" >> ~/.daysuntil.deadlines
+echo "$(~/bin/daysuntil 2017-09-16) days - Saw Works beer mile" > ~/.daysuntil.deadlines
 echo "$(~/bin/daysuntil 2017-10-28) days - Farragut 13.1" >> ~/.daysuntil.deadlines
 echo "$(~/bin/daysuntil 2017-12-18) days - CSNS review" >> ~/.daysuntil.deadlines
 echo "$(~/bin/daysuntil 2018-03-10) days - Cruise" >> ~/.daysuntil.deadlines
+echo "$(~/bin/daysuntil 2020-04-12) days - Python2 is dead" >> ~/.daysuntil.deadlines
