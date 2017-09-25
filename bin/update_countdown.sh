5ab1f5900605117b09dc81e3f793f9a39c05ca35
Pete Peterson <petersonpf@ornl.gov>
2017-09-25 15:44:38 -0400

    Add more events

--- bin/update_countdown.sh
+++ bin/update_countdown.sh
@@ -1,7 +1,10 @@
 #!/bin/sh
+echo "$(~/bin/daysuntil 2017-10-09) days - Mantid 3.11" > ~/.daysuntil.deadlines
 # https://fedoraproject.org/wiki/Releases/27/Schedule
-echo "$(~/bin/daysuntil 2017-10-24) days - Fedora 27" > ~/.daysuntil.deadlines
+echo "$(~/bin/daysuntil 2017-10-24) days - Fedora 27" >> ~/.daysuntil.deadlines
 echo "$(~/bin/daysuntil 2017-10-28) days - Farragut 13.1" >> ~/.daysuntil.deadlines
 echo "$(~/bin/daysuntil 2017-12-18) days - CSNS review" >> ~/.daysuntil.deadlines
+echo "$(~/bin/daysuntil 2018-02-05) days - Mantid 3.12" >> ~/.daysuntil.deadlines
 echo "$(~/bin/daysuntil 2018-03-10) days - Cruise" >> ~/.daysuntil.deadlines
+echo "$(~/bin/daysuntil 2018-03-25) days - CHKM" >> ~/.daysuntil.deadlines
 echo "$(~/bin/daysuntil 2020-04-12) days - Python2 is dead" >> ~/.daysuntil.deadlines
