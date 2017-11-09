9f81ff01f6a852436edb714eb3c3ad82464b3bbd
Pete Peterson <petersonpf@ornl.gov>
2017-11-09 14:18:22 -0500

    Add ubuntu 18.04

--- bin/update_countdown.sh
+++ bin/update_countdown.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
-echo "$(~/bin/daysuntil 2017-10-28) days - Farragut 13.1" > ~/.daysuntil.deadlines
 # https://fedoraproject.org/wiki/Releases/27/Schedule
-echo "$(~/bin/daysuntil 2017-11-28) days - Fedora 27" >> ~/.daysuntil.deadlines
+echo "$(~/bin/daysuntil 2017-11-28) days - Fedora 27" > ~/.daysuntil.deadlines
 echo "$(~/bin/daysuntil 2017-12-18) days - CSNS review" >> ~/.daysuntil.deadlines
 echo "$(~/bin/daysuntil 2018-02-05) days - Mantid 3.12" >> ~/.daysuntil.deadlines
 echo "$(~/bin/daysuntil 2018-03-10) days - Cruise" >> ~/.daysuntil.deadlines
 echo "$(~/bin/daysuntil 2018-03-25) days - CHKM" >> ~/.daysuntil.deadlines
+echo "$(~/bin/daysuntil 2018-04-01) days - Ubuntu 18.03" >> ~/.daysuntil.deadlines
 echo "$(~/bin/daysuntil 2020-04-12) days - Python2 is dead" >> ~/.daysuntil.deadlines
