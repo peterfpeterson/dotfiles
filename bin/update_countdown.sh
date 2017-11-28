d6f678d321b830591f2beec0c5ad6edb13add469
Pete Peterson <petersonpf@ornl.gov>
2017-11-28 11:53:26 -0500

    Add ubuntu beta

--- bin/update_countdown.sh
+++ bin/update_countdown.sh
@@ -5,5 +5,6 @@ echo "$(~/bin/daysuntil 2018-01-29) days - Mantid workshops" >> ~/.daysuntil.dea
 echo "$(~/bin/daysuntil 2018-02-05) days - Mantid 3.12" >> ~/.daysuntil.deadlines
 echo "$(~/bin/daysuntil 2018-03-10) days - Cruise" >> ~/.daysuntil.deadlines
 echo "$(~/bin/daysuntil 2018-03-25) days - CHKM" >> ~/.daysuntil.deadlines
-echo "$(~/bin/daysuntil 2018-04-01) days - Ubuntu 18.03" >> ~/.daysuntil.deadlines
+echo "$(~/bin/daysuntil 2018-03-08) days - Ubuntu 18.04 beta" >> ~/.daysuntil.deadlines
+echo "$(~/bin/daysuntil 2018-04-26) days - Ubuntu 18.04" >> ~/.daysuntil.deadlines
 echo "$(~/bin/daysuntil 2020-04-12) days - Python2 is dead" >> ~/.daysuntil.deadlines
