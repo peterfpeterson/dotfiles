db3ee6008849d820745ce578cef48ebe9077c798
Pete Peterson <petersonpf@ornl.gov>
2018-01-30 13:27:52 -0500

    Remove mantid workshop

--- bin/update_countdown.sh
+++ bin/update_countdown.sh
@@ -1,8 +1,6 @@
 #!/bin/sh
 # https://fedoraproject.org/wiki/Releases/27/Schedule
-echo "$(~/bin/daysuntil 2017-12-18) days - CSNS review" > ~/.daysuntil.deadlines
-echo "$(~/bin/daysuntil 2018-01-29) days - Mantid workshops" >> ~/.daysuntil.deadlines
-echo "$(~/bin/daysuntil 2018-02-05) days - Mantid 3.12" >> ~/.daysuntil.deadlines
+echo "$(~/bin/daysuntil 2018-02-05) days - Mantid 3.12" > ~/.daysuntil.deadlines
 echo "$(~/bin/daysuntil 2018-03-10) days - Cruise" >> ~/.daysuntil.deadlines
 echo "$(~/bin/daysuntil 2018-03-25) days - CHKM" >> ~/.daysuntil.deadlines
 echo "$(~/bin/daysuntil 2018-03-08) days - Ubuntu 18.04 beta" >> ~/.daysuntil.deadlines
