e8a5c7d01349f3938b860b325002700a2e963894
Pete Peterson <petersonpf@ornl.gov>
2018-02-13 14:23:21 -0500

    Update deadlines

--- bin/update_countdown.sh
+++ bin/update_countdown.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 # https://fedoraproject.org/wiki/Releases/27/Schedule
-echo "$(~/bin/daysuntil 2018-02-05) days - Mantid 3.12" > ~/.daysuntil.deadlines
+echo "$(~/bin/daysuntil 2018-02-23) days - Mantid 3.12 code freeze" > ~/.daysuntil.deadlines
 echo "$(~/bin/daysuntil 2018-03-10) days - Cruise" >> ~/.daysuntil.deadlines
+echo "$(~/bin/daysuntil 2018-03-19) days - Mantid 3.12" >> ~/.daysuntil.deadlines
 echo "$(~/bin/daysuntil 2018-03-25) days - CHKM" >> ~/.daysuntil.deadlines
 echo "$(~/bin/daysuntil 2018-03-08) days - Ubuntu 18.04 beta" >> ~/.daysuntil.deadlines
 echo "$(~/bin/daysuntil 2018-04-26) days - Ubuntu 18.04" >> ~/.daysuntil.deadlines
