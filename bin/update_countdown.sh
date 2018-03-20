6461468d872a2686fabb41374c369af1b1705339
Pete Peterson <petersonpf@ornl.gov>
2018-03-20 14:03:14 -0400

    Remove past events

--- bin/update_countdown.sh
+++ bin/update_countdown.sh
@@ -1,9 +1,5 @@
 #!/bin/sh
 # https://fedoraproject.org/wiki/Releases/27/Schedule
-echo "$(~/bin/daysuntil 2018-02-23) days - Mantid 3.12 code freeze" > ~/.daysuntil.deadlines
-echo "$(~/bin/daysuntil 2018-03-10) days - Cruise" >> ~/.daysuntil.deadlines
-echo "$(~/bin/daysuntil 2018-03-19) days - Mantid 3.12" >> ~/.daysuntil.deadlines
-echo "$(~/bin/daysuntil 2018-03-25) days - CHKM" >> ~/.daysuntil.deadlines
-echo "$(~/bin/daysuntil 2018-03-08) days - Ubuntu 18.04 beta" >> ~/.daysuntil.deadlines
+echo "$(~/bin/daysuntil 2018-03-25) days - CHKM" > ~/.daysuntil.deadlines
 echo "$(~/bin/daysuntil 2018-04-26) days - Ubuntu 18.04" >> ~/.daysuntil.deadlines
 echo "$(~/bin/daysuntil 2020-04-12) days - Python2 is dead" >> ~/.daysuntil.deadlines
