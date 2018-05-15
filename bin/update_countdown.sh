0c42f6d1643789f77a2d755564111e8762971d5d
Pete Peterson <petersonpf@ornl.gov>
2018-05-15 10:03:18 -0400

    Removing things from the past

--- bin/update_countdown.sh
+++ bin/update_countdown.sh
@@ -1,11 +1,5 @@
 #!/bin/sh
-# https://fedoraproject.org/wiki/Releases/27/Schedule
-echo "$(~/bin/daysuntil 2018-04-26) days - Ubuntu 18.04" > ~/.daysuntil.deadlines
-echo "$(~/bin/daysuntil 2018-05-14) days - Ubuntu 18.04 at ORNL" >> ~/.daysuntil.deadlines
-echo "$(~/bin/daysuntil 2018-05-11) days - Kentuckiana Ragnar" >> ~/.daysuntil.deadlines
-echo "$(~/bin/daysuntil 2018-05-13) days - Mother's day" >> ~/.daysuntil.deadlines
-echo "$(~/bin/daysuntil 2018-05-15) days - Software expo" >> ~/.daysuntil.deadlines
-echo "$(~/bin/daysuntil 2018-05-18) days - 8th grade graduation" >> ~/.daysuntil.deadlines
+echo "$(~/bin/daysuntil 2018-05-18) days - 8th grade graduation" > ~/.daysuntil.deadlines
 echo "$(~/bin/daysuntil 2018-06-29) days - mantid v3.13 code freeze" >> ~/.daysuntil.deadlines
 echo "$(~/bin/daysuntil 2018-07-23) days - mantid v3.13" >> ~/.daysuntil.deadlines
 echo "$(~/bin/daysuntil 2018-11-23) days - mantid v3.14 code freeze" >> ~/.daysuntil.deadlines
