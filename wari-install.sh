744d700f8877ed7e7eb0794512b3a588d6f2f1b6
Pete Peterson <petersonpf@ornl.gov>
2018-05-21 12:09:14 -0400

    Twiddle with things for rhel7

--- wari-install.sh
+++ wari-install.sh
@@ -10,7 +10,7 @@ add_repo elrepo epel
 add_repo mantid
 add_repo rpmfusion-free rpmfusion-nonfree
 #add_repo sns
-add_repo nvidia
+#add_repo nvidia
 
 if [ ! -f ${REPO_DIR}/_copr_peterfpeterson-morebin.repo ]; then
     add_copr_repo peterfpeterson/morebin
@@ -28,6 +28,7 @@ add_package ccache
 add_package colordiff
 add_package conky
 add_package direnv
+add_package highlight
 add_package ninja-build
 add_package python2-argcomplete
 add_package python3-argcomplete
@@ -50,12 +51,12 @@ add_package terminology
 add_package hidapi-devel
 add_package source-highlight
 # things for awesome window manager
-add_package awesome
-add_package vicious # widget set
-add_package i3lock # screen lock
+#add_package awesome
+#add_package vicious # widget set
+#add_package i3lock # screen lock
 # smart card reader
-add_package opensc
-add_package pcsc-tools
-add_package pcsc-lite-ccid
+#add_package opensc
+#add_package pcsc-tools
+#add_package pcsc-lite-ccid
 
 echo "Done"
