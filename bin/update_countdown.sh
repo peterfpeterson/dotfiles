3215f93ee52af8d40cead7b9eec0a4bfe8b476a7
Pete Peterson <petersonpf@ornl.gov>
2018-05-24 16:54:40 -0400

    Add triathlon

--- bin/update_countdown.sh
+++ bin/update_countdown.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
-echo "$(~/bin/daysuntil 2018-05-18) days - 8th grade graduation" > ~/.daysuntil.deadlines
-echo "$(~/bin/daysuntil 2018-06-29) days - mantid v3.13 code freeze" >> ~/.daysuntil.deadlines
+echo "$(~/bin/daysuntil 2018-06-29) days - mantid v3.13 code freeze" > ~/.daysuntil.deadlines
+echo "$(~/bin/daysuntil 2018-06-30) days - Secret City Tri" >> ~/.daysuntil.deadlines
 echo "$(~/bin/daysuntil 2018-07-23) days - mantid v3.13" >> ~/.daysuntil.deadlines
 echo "$(~/bin/daysuntil 2018-11-23) days - mantid v3.14 code freeze" >> ~/.daysuntil.deadlines
 echo "$(~/bin/daysuntil 2018-12-17) days - mantid v3.14" >> ~/.daysuntil.deadlines
