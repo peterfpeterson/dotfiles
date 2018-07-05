3e0fd3cefbd3bc7912c8c7b9871cc6caa85de7e5
Pete Peterson <petersonpf@ornl.gov>
2018-07-05 15:09:03 -0400

    Remove things in the past

--- bin/update_countdown.sh
+++ bin/update_countdown.sh
@@ -1,8 +1,5 @@
 #!/bin/sh
-echo "$(~/bin/daysuntil 2018-06-28) days - Rhys arrives" > ~/.daysuntil.deadlines
-echo "$(~/bin/daysuntil 2018-06-29) days - mantid v3.13 code freeze" >> ~/.daysuntil.deadlines
-echo "$(~/bin/daysuntil 2018-06-30) days - Secret City Tri" >> ~/.daysuntil.deadlines
-echo "$(~/bin/daysuntil 2018-07-23) days - mantid v3.13" >> ~/.daysuntil.deadlines
+echo "$(~/bin/daysuntil 2018-07-23) days - mantid v3.13" > ~/.daysuntil.deadlines
 echo "$(~/bin/daysuntil 2018-07-26) days - Rhys leaves" >> ~/.daysuntil.deadlines
 echo "$(~/bin/daysuntil 2018-11-23) days - mantid v3.14 code freeze" >> ~/.daysuntil.deadlines
 echo "$(~/bin/daysuntil 2018-12-17) days - mantid v3.14" >> ~/.daysuntil.deadlines
