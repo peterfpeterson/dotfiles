2b510b7c9ce9fbdc0c9deb541b0f987ca9e18c9b
Pete Peterson <petersonpf@ornl.gov>
2018-07-20 10:54:40 -0400

    Add new event

--- bin/update_countdown.sh
+++ bin/update_countdown.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 echo "$(~/bin/daysuntil 2018-07-23) days - mantid v3.13" > ~/.daysuntil.deadlines
 echo "$(~/bin/daysuntil 2018-07-26) days - Rhys leaves" >> ~/.daysuntil.deadlines
+echo "$(~/bin/daysuntil 2018-08-16) days - Insurance verification" >> ~/.daysuntil.deadlines
 echo "$(~/bin/daysuntil 2018-11-23) days - mantid v3.14 code freeze" >> ~/.daysuntil.deadlines
 echo "$(~/bin/daysuntil 2018-12-17) days - mantid v3.14" >> ~/.daysuntil.deadlines
 echo "$(~/bin/daysuntil 2020-04-12) days - Python2 is dead" >> ~/.daysuntil.deadlines
