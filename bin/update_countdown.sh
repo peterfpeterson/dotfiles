3dd70c6fda6f6b0cc0a828e3e8fb695ef54a29cf
Pete Peterson <petersonpf@ornl.gov>
2018-07-24 11:21:37 -0400

    Add conferences

--- bin/update_countdown.sh
+++ bin/update_countdown.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
 echo "$(~/bin/daysuntil 2018-07-23) days - mantid v3.13" > ~/.daysuntil.deadlines
 echo "$(~/bin/daysuntil 2018-07-26) days - Rhys leaves" >> ~/.daysuntil.deadlines
-echo "$(~/bin/daysuntil 2018-08-16) days - Insurance verification" >> ~/.daysuntil.deadlines
+echo "$(~/bin/daysuntil 2018-08-16) days - Upload stats corrections" >> ~/.daysuntil.deadlines
+echo "$(~/bin/daysuntil 2018-10-22) days - NOBUGS" >> ~/.daysuntil.deadlines
+echo "$(~/bin/daysuntil 2018-11-15) days - CSNS review" >> ~/.daysuntil.deadlines
 echo "$(~/bin/daysuntil 2018-11-23) days - mantid v3.14 code freeze" >> ~/.daysuntil.deadlines
 echo "$(~/bin/daysuntil 2018-12-17) days - mantid v3.14" >> ~/.daysuntil.deadlines
 echo "$(~/bin/daysuntil 2020-04-12) days - Python2 is dead" >> ~/.daysuntil.deadlines
