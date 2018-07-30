2dd9bca155d4013ba9222a1333cff5be1d687406
Pete Peterson <petersonpf@ornl.gov>
2018-07-30 16:45:44 -0400

    Update deadlines

--- bin/update_countdown.sh
+++ bin/update_countdown.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
-echo "$(~/bin/daysuntil 2018-07-23) days - mantid v3.13" > ~/.daysuntil.deadlines
-echo "$(~/bin/daysuntil 2018-07-26) days - Rhys leaves" >> ~/.daysuntil.deadlines
-echo "$(~/bin/daysuntil 2018-08-16) days - Upload stats corrections" >> ~/.daysuntil.deadlines
+echo "$(~/bin/daysuntil 2018-08-16) days - Upload stats corrections" > ~/.daysuntil.deadlines
+echo "$(~/bin/daysuntil 2018-08-28) days - Smoky Mountain Conference" >> ~/.daysuntil.deadlines
 echo "$(~/bin/daysuntil 2018-10-22) days - NOBUGS" >> ~/.daysuntil.deadlines
 echo "$(~/bin/daysuntil 2018-11-15) days - CSNS review" >> ~/.daysuntil.deadlines
 echo "$(~/bin/daysuntil 2018-11-23) days - mantid v3.14 code freeze" >> ~/.daysuntil.deadlines
