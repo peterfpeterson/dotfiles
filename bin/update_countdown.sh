31f1fa7f8d82f8140606e10d82a905dd1b315577
Pete Peterson <petersonpf@ornl.gov>
2018-09-12 13:14:40 -0400

    Remove passed deadlines

--- bin/update_countdown.sh
+++ bin/update_countdown.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
-echo "$(~/bin/daysuntil 2018-08-16) days - Upload stats corrections" > ~/.daysuntil.deadlines
-echo "$(~/bin/daysuntil 2018-08-28) days - Smoky Mountain Conference" >> ~/.daysuntil.deadlines
+echo "$(~/bin/daysuntil 2018-09-17) days - NOBUGS abstracts" > ~/.daysuntil.deadlines
 echo "$(~/bin/daysuntil 2018-10-22) days - NOBUGS" >> ~/.daysuntil.deadlines
 echo "$(~/bin/daysuntil 2018-11-15) days - CSNS review" >> ~/.daysuntil.deadlines
 echo "$(~/bin/daysuntil 2018-11-23) days - mantid v3.14 code freeze" >> ~/.daysuntil.deadlines
 echo "$(~/bin/daysuntil 2018-12-17) days - mantid v3.14" >> ~/.daysuntil.deadlines
+echo "$(~/bin/daysuntil 2019-03-31) days - CHKM" >> ~/.daysuntil.deadlines
 echo "$(~/bin/daysuntil 2020-04-12) days - Python2 is dead" >> ~/.daysuntil.deadlines
