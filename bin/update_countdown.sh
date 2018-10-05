8e82c0cf1f7301194fe3cc3f6d9e878de9f28006
Pete Peterson <petersonpf@ornl.gov>
2018-10-05 08:40:37 -0400

    Remove past events

--- bin/update_countdown.sh
+++ bin/update_countdown.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
-echo "$(~/bin/daysuntil 2018-09-17) days - NOBUGS abstracts" > ~/.daysuntil.deadlines
-echo "$(~/bin/daysuntil 2018-10-22) days - NOBUGS" >> ~/.daysuntil.deadlines
+echo "$(~/bin/daysuntil 2018-10-22) days - NOBUGS" > ~/.daysuntil.deadlines
 echo "$(~/bin/daysuntil 2018-11-15) days - CSNS review" >> ~/.daysuntil.deadlines
 echo "$(~/bin/daysuntil 2018-11-23) days - mantid v3.14 code freeze" >> ~/.daysuntil.deadlines
 echo "$(~/bin/daysuntil 2018-12-17) days - mantid v3.14" >> ~/.daysuntil.deadlines
