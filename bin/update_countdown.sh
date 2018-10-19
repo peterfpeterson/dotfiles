e1c9ad630bbe8af424c205b0cdcfc25e7de61869
Pete Peterson <petersonpf@ornl.gov>
2018-10-19 11:06:08 -0400

    Update list of events

--- bin/update_countdown.sh
+++ bin/update_countdown.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
-echo "$(~/bin/daysuntil 2018-10-22) days - NOBUGS" > ~/.daysuntil.deadlines
-echo "$(~/bin/daysuntil 2018-11-15) days - CSNS review" >> ~/.daysuntil.deadlines
-echo "$(~/bin/daysuntil 2018-11-23) days - mantid v3.14 code freeze" >> ~/.daysuntil.deadlines
-echo "$(~/bin/daysuntil 2018-12-17) days - mantid v3.14" >> ~/.daysuntil.deadlines
+echo "$(~/bin/daysuntil 2018-11-17) days - Secret City Half" > ~/.daysuntil.deadlines
+echo "$(~/bin/daysuntil 2018-03-01) days - mantid v3.14 code freeze" >> ~/.daysuntil.deadlines
+echo "$(~/bin/daysuntil 2018-03-25) days - mantid v3.14 release" >> ~/.daysuntil.deadlines
 echo "$(~/bin/daysuntil 2019-03-31) days - CHKM" >> ~/.daysuntil.deadlines
 echo "$(~/bin/daysuntil 2020-04-12) days - Python2 is dead" >> ~/.daysuntil.deadlines
