f459ea2447c3bca9b93017620e3e4c108006638d
Pete Peterson <petersonpf@ornl.gov>
2018-11-19 12:55:03 -0500

    Remove past event

--- bin/update_countdown.sh
+++ bin/update_countdown.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
-echo "$(~/bin/daysuntil 2018-11-17) days - Secret City Half" > ~/.daysuntil.deadlines
-echo "$(~/bin/daysuntil 2018-12-10) days - webinar" >> ~/.daysuntil.deadlines
+echo "$(~/bin/daysuntil 2018-12-10) days - webinar" > ~/.daysuntil.deadlines
 echo "$(~/bin/daysuntil 2019-03-01) days - mantid v3.14 code freeze" >> ~/.daysuntil.deadlines
 echo "$(~/bin/daysuntil 2019-03-25) days - mantid v3.14 release" >> ~/.daysuntil.deadlines
 echo "$(~/bin/daysuntil 2019-03-31) days - CHKM" >> ~/.daysuntil.deadlines
