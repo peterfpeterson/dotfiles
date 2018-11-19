e4f7a2c6e517d86beeb4fbab4f2a9163f03e911c
Pete Peterson <petersonpf@ornl.gov>
2018-11-19 16:54:33 -0500

    Add big items

--- bin/update_countdown.sh
+++ bin/update_countdown.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 echo "$(~/bin/daysuntil 2018-12-10) days - webinar" > ~/.daysuntil.deadlines
+echo "$(~/bin/daysuntil 2019-01-15) days - PG3 absorption" >> ~/.daysuntil.deadlines
 echo "$(~/bin/daysuntil 2019-03-01) days - mantid v3.14 code freeze" >> ~/.daysuntil.deadlines
 echo "$(~/bin/daysuntil 2019-03-25) days - mantid v3.14 release" >> ~/.daysuntil.deadlines
 echo "$(~/bin/daysuntil 2019-03-31) days - CHKM" >> ~/.daysuntil.deadlines
