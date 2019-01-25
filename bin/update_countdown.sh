7af0a2b17afc2e8636793ee9f43bebd8900b127b
Pete Peterson <petersonpf@ornl.gov>
2019-01-25 09:29:23 -0500

    Add races to countdown

--- bin/update_countdown.sh
+++ bin/update_countdown.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
-echo "$(~/bin/daysuntil 2019-01-15) days - PG3 absorption" > ~/.daysuntil.deadlines
-echo "$(~/bin/daysuntil 2019-01-29) days - mantid PMB" >> ~/.daysuntil.deadlines
+echo "$(~/bin/daysuntil 2019-01-29) days - mantid PMB" > ~/.daysuntil.deadlines
 echo "$(~/bin/daysuntil 2019-03-01) days - mantid v3.14 code freeze" >> ~/.daysuntil.deadlines
 echo "$(~/bin/daysuntil 2019-03-25) days - mantid v3.14 release" >> ~/.daysuntil.deadlines
 echo "$(~/bin/daysuntil 2019-03-31) days - CHKM" >> ~/.daysuntil.deadlines
 echo "$(~/bin/daysuntil 2019-04-08) days - mantid workshops" >> ~/.daysuntil.deadlines
+echo "$(~/bin/daysuntil 2019-06-29) days - Secret City Tri" >> ~/.daysuntil.deadlines
+echo "$(~/bin/daysuntil 2019-10-13) days - TN Toughman" >> ~/.daysuntil.deadlines
 echo "$(~/bin/daysuntil 2020-04-12) days - Python2 is dead" >> ~/.daysuntil.deadlines
