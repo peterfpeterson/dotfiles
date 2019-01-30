1796e13624d854b91f7eb57b603ea17c16fe53f7
Pete Peterson <petersonpf@ornl.gov>
2019-01-30 08:40:39 -0500

    Correct date for mantid meetings

--- bin/update_countdown.sh
+++ bin/update_countdown.sh
@@ -2,7 +2,7 @@
 echo "$(~/bin/daysuntil 2019-03-01) days - mantid v3.14 code freeze" > ~/.daysuntil.deadlines
 echo "$(~/bin/daysuntil 2019-03-25) days - mantid v3.14 release" >> ~/.daysuntil.deadlines
 echo "$(~/bin/daysuntil 2019-03-31) days - CHKM" >> ~/.daysuntil.deadlines
-echo "$(~/bin/daysuntil 2019-04-08) days - mantid workshops" >> ~/.daysuntil.deadlines
+echo "$(~/bin/daysuntil 2019-04-01) days - mantid workshops" >> ~/.daysuntil.deadlines
 echo "$(~/bin/daysuntil 2019-06-29) days - Secret City Tri" >> ~/.daysuntil.deadlines
 echo "$(~/bin/daysuntil 2019-10-13) days - TN Toughman" >> ~/.daysuntil.deadlines
 echo "$(~/bin/daysuntil 2020-04-12) days - Python2 is dead" >> ~/.daysuntil.deadlines
