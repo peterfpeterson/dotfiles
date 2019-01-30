c7c88d83c9960b97b842a83dace8dccf7da247e1
Pete Peterson <petersonpf@ornl.gov>
2019-01-30 08:38:09 -0500

    Remove bits that happen in the past

--- bin/update_countdown.sh
+++ bin/update_countdown.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
-echo "$(~/bin/daysuntil 2019-01-29) days - mantid PMB" > ~/.daysuntil.deadlines
-echo "$(~/bin/daysuntil 2019-03-01) days - mantid v3.14 code freeze" >> ~/.daysuntil.deadlines
+echo "$(~/bin/daysuntil 2019-03-01) days - mantid v3.14 code freeze" > ~/.daysuntil.deadlines
 echo "$(~/bin/daysuntil 2019-03-25) days - mantid v3.14 release" >> ~/.daysuntil.deadlines
 echo "$(~/bin/daysuntil 2019-03-31) days - CHKM" >> ~/.daysuntil.deadlines
 echo "$(~/bin/daysuntil 2019-04-08) days - mantid workshops" >> ~/.daysuntil.deadlines
