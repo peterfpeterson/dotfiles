a1061ee21a4406ed1ed137e1d4729b399f909af0
Pete Peterson <petersonpf@ornl.gov>
2019-04-09 13:05:42 -0400

    Remove past event

--- bin/update_countdown.sh
+++ bin/update_countdown.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
-echo "$(~/bin/daysuntil 2019-03-31) days - CHKM" > ~/.daysuntil.deadlines
-echo "$(~/bin/daysuntil 2019-04-01) days - mantid workshops" >> ~/.daysuntil.deadlines
+echo "$(~/bin/daysuntil 2019-05-05) days - Flying Pig" > ~/.daysuntil.deadlines
 echo "$(~/bin/daysuntil 2019-06-29) days - Secret City Tri" >> ~/.daysuntil.deadlines
 echo "$(~/bin/daysuntil 2019-10-13) days - TN Toughman" >> ~/.daysuntil.deadlines
 echo "$(~/bin/daysuntil 2020-04-12) days - Python2 is dead" >> ~/.daysuntil.deadlines
