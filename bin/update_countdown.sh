b0dcab3a5b209e96d4778a3213bef972fce1defe
Pete Peterson <petersonpf@ornl.gov>
2019-06-07 11:33:32 -0400

    Add events

--- bin/update_countdown.sh
+++ bin/update_countdown.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
 echo "$(~/bin/daysuntil 2019-06-29) days - Secret City Tri" > ~/.daysuntil.deadlines
+echo "$(~/bin/daysuntil 2019-06-30) days - CHKM price increase" >> ~/.daysuntil.deadlines
 echo "$(~/bin/daysuntil 2019-10-13) days - TN Toughman" >> ~/.daysuntil.deadlines
+echo "$(~/bin/daysuntil 2020-03-29) days - Knoxville Marathon" >> ~/.daysuntil.deadlines
 echo "$(~/bin/daysuntil 2020-04-12) days - Python2 is dead" >> ~/.daysuntil.deadlines
