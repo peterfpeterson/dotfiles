def9cfbcd8cfd39bc757dc6c110eb3ab7dfa95e8
Pete Peterson <petersonpf@ornl.gov>
2019-08-06 09:23:42 -0400

    Add event

--- bin/update_countdown.sh
+++ bin/update_countdown.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-echo "$(~/bin/daysuntil 2019-08-05) days - First day of school" > ~/.daysuntil.deadlines
+echo "$(~/bin/daysuntil 2019-08-16) days - Girls' weekend" > ~/.daysuntil.deadlines
 echo "$(~/bin/daysuntil 2019-08-23) days - Alex moves to MTSU" >> ~/.daysuntil.deadlines
 echo "$(~/bin/daysuntil 2019-08-27) days - Smoky Mountain Conference" >> ~/.daysuntil.deadlines
 echo "$(~/bin/daysuntil 2019-08-30) days - Bike to Elkmont" >> ~/.daysuntil.deadlines
