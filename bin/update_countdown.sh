d42ed63430cfe3939965bdee0a831f88edab2207
Pete Peterson <petersonpf@ornl.gov>
2019-08-23 13:40:15 -0400

    Remove past date

--- bin/update_countdown.sh
+++ bin/update_countdown.sh
@@ -1,7 +1,5 @@
 #!/bin/sh
-echo "$(~/bin/daysuntil 2019-08-16) days - Girls' weekend" > ~/.daysuntil.deadlines
-echo "$(~/bin/daysuntil 2019-08-23) days - Alex moves to MTSU" >> ~/.daysuntil.deadlines
-echo "$(~/bin/daysuntil 2019-08-27) days - Smoky Mountain Conference" >> ~/.daysuntil.deadlines
+echo "$(~/bin/daysuntil 2019-08-27) days - Smoky Mountain Conference" > ~/.daysuntil.deadlines
 echo "$(~/bin/daysuntil 2019-08-30) days - Bike to Elkmont" >> ~/.daysuntil.deadlines
 echo "$(~/bin/daysuntil 2019-10-13) days - TN Toughman" >> ~/.daysuntil.deadlines
 echo "$(~/bin/daysuntil 2020-03-29) days - Knoxville Marathon" >> ~/.daysuntil.deadlines
