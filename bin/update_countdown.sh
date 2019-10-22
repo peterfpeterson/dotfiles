e1fc54e4e2193ea76201b9d2c4c6618e78bcf8f6
Pete Peterson <petersonpf@ornl.gov>
2019-10-22 13:00:22 -0400

    Remove things from the past

--- bin/update_countdown.sh
+++ bin/update_countdown.sh
@@ -1,7 +1,5 @@
 #!/bin/sh
-echo "$(~/bin/daysuntil 2019-10-13) days - TN Toughman" > ~/.daysuntil.deadlines
-echo "$(~/bin/daysuntil 2019-10-14) days - ICANS conference" >> ~/.daysuntil.deadlines
-echo "$(~/bin/daysuntil 2019-10-22) days - HFIR startup" >> ~/.daysuntil.deadlines
+echo "$(~/bin/daysuntil 2019-11-01) days - HFIR startup" > ~/.daysuntil.deadlines
 echo "$(~/bin/daysuntil 2020-03-29) days - Knoxville Marathon" >> ~/.daysuntil.deadlines
 echo "$(~/bin/daysuntil 2020-04-12) days - Python2 is dead" >> ~/.daysuntil.deadlines
 
