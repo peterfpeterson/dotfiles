1126db793e6d4acb058760b15a7f95ed81228b03
Pete Peterson <petersonpf@ornl.gov>
2019-10-23 12:57:57 -0400

    Add new dates

--- bin/update_countdown.sh
+++ bin/update_countdown.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
-echo "$(~/bin/daysuntil 2019-11-01) days - HFIR startup" > ~/.daysuntil.deadlines
+echo "$(~/bin/daysuntil 2019-10-25) days - Mantid code freeze" > ~/.daysuntil.deadlines
+echo "$(~/bin/daysuntil 2019-11-18) days - Mantid 4.2" >> ~/.daysuntil.deadlines
+echo "$(~/bin/daysuntil 2019-10-29) days - HFIR startup" >> ~/.daysuntil.deadlines
 echo "$(~/bin/daysuntil 2020-03-29) days - Knoxville Marathon" >> ~/.daysuntil.deadlines
 echo "$(~/bin/daysuntil 2020-04-12) days - Python2 is dead" >> ~/.daysuntil.deadlines
 
