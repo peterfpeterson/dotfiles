e555c73acfcdaa828b280d27968992eac98761d4
Pete Peterson <petersonpf@ornl.gov>
2019-10-30 08:26:30 -0400

    Remove past event

--- bin/update_countdown.sh
+++ bin/update_countdown.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
 echo "$(~/bin/daysuntil 2019-11-18) days - Mantid 4.2" > ~/.daysuntil.deadlines
-echo "$(~/bin/daysuntil 2019-10-29) days - HFIR startup" >> ~/.daysuntil.deadlines
 echo "$(~/bin/daysuntil 2020-02-01) days - LDRD proposals due" >> ~/.daysuntil.deadlines
 echo "$(~/bin/daysuntil 2020-03-29) days - Knoxville Marathon" >> ~/.daysuntil.deadlines
 echo "$(~/bin/daysuntil 2020-04-12) days - Python2 is dead" >> ~/.daysuntil.deadlines
