fe9872a893a04f5e63e6e8a0038bf96115429efa
Pete Peterson <petersonpf@ornl.gov>
2019-12-04 08:31:52 -0500

    New mantid version

--- bin/update_countdown.sh
+++ bin/update_countdown.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-echo "$(~/bin/daysuntil 2019-11-18) days - Mantid 4.2" > ~/.daysuntil.deadlines
+echo "$(~/bin/daysuntil 2020-03-30) days - Mantid 4.3" > ~/.daysuntil.deadlines
 echo "$(~/bin/daysuntil 2020-02-01) days - LDRD proposals due" >> ~/.daysuntil.deadlines
 echo "$(~/bin/daysuntil 2020-03-29) days - Knoxville Marathon" >> ~/.daysuntil.deadlines
 echo "$(~/bin/daysuntil 2020-04-12) days - Python2 is dead" >> ~/.daysuntil.deadlines
