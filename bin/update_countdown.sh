f27532526cf1ade4ddcf141957d1b97d6275556f
Pete Peterson <petersonpf@ornl.gov>
2020-01-27 10:38:29 -0500

    Add wowoha talk deadline

--- bin/update_countdown.sh
+++ bin/update_countdown.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 echo "$(~/bin/daysuntil 2020-03-30) days - Mantid 4.3" > ~/.daysuntil.deadlines
 echo "$(~/bin/daysuntil 2020-02-01) days - LDRD proposals due" >> ~/.daysuntil.deadlines
+echo "$(~/bin/daysuntil 2020-03-06) days - WoWoHa talk" >> ~/.daysuntil.deadlines
+echo "$(~/bin/daysuntil 2020-03-10) days - WoWoHa conference" >> ~/.daysuntil.deadlines
 echo "$(~/bin/daysuntil 2020-03-29) days - Knoxville Marathon" >> ~/.daysuntil.deadlines
 echo "$(~/bin/daysuntil 2020-04-12) days - Python2 is dead" >> ~/.daysuntil.deadlines
 
