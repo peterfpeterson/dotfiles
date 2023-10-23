7e92925709624aeba2e092428a8df83817315a15
Pete Peterson <peterfpeterson@gmail.com>
2023-10-23 13:50:16 -0400

    Add config step for vtune

--- bin/perf_permissions.sh
+++ bin/perf_permissions.sh
@@ -7,3 +7,7 @@ echo "0" | sudo tee /proc/sys/kernel/kptr_restrict
 echo "-1" | sudo tee /proc/sys/kernel/perf_event_paranoid
 sudo chown `whoami` /sys/kernel/debug/tracing/uprobe_events
 sudo chmod a+rw /sys/kernel/debug/tracing/uprobe_events
+
+# Taken from vtune's docs
+# https://www.intel.com/content/www/us/en/docs/vtune-profiler/user-guide/2023-0/linux-targets.html
+sudo sysctl -w kernel.yama.ptrace_scope=0
