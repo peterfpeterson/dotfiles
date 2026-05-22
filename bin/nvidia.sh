8dcd64b8accf3a6664bad77bf742557a5a53fdb0
Pete Peterson <peterfpeterson@gmail.com>
2026-05-22 10:22:35 -0400

    Be more resilient to errors in getting information

--- bin/nvidia.sh
+++ bin/nvidia.sh
@@ -1,25 +1,28 @@
 #!/usr/bin/env bash
 
-OUTPUT_FILE=~/.cache/nvidia_conky.txt
-
 if [ $(lsmod | grep 'nvidia' -c) ]; then
     # GPU name
     #nvidia-smi --query-gpu=name --format=csv,noheader > "${OUTPUT_FILE}"
 
     # ask for information
     EVERYTHING=( $(nvidia-smi --query-gpu=utilization.gpu,temperature.gpu,memory.used,memory.free,memory.total --format=noheader,nounits --format=noheader,nounits | sed 's/,/ /g') )
-    # in percent
-    UTILIZATION="${EVERYTHING[0]}"
-    # in C
-    TEMPERATURE="${EVERYTHING[1]}"
-    # in MiB
-    MEMORY_USED="${EVERYTHING[2]}"
-    MEMORY_FREE="${EVERYTHING[3]}"
-    MEMORY_TOTAL="${EVERYTHING[4]}"
+
+    if [[ "${EVERYTHING}" == "Failed"* ]]; then
+	nvidia-smi --query-gpu=utilization.gpu,temperature.gpu,memory.used,memory.free,memory.total --format=noheader,nounits --format=noheader,nounits | head -n 2 | head -c 40
+    else
+	# in percent
+	UTILIZATION="${EVERYTHING[0]}"
+	# in C
+	TEMPERATURE="${EVERYTHING[1]}"
+	# in MiB
+	MEMORY_USED="${EVERYTHING[2]}"
+	MEMORY_FREE="${EVERYTHING[3]}"
+	MEMORY_TOTAL="${EVERYTHING[4]}"
 
     # write everything out
-    echo "\${color2}Temperature\$color ${TEMPERATURE}°C\${alignr}\${color2}Utilization\$color \${alignr}${UTILIZATION}%"
-    echo "\${color2}Memory\$color\${alignr}${MEMORY_USED} + ${MEMORY_FREE} = ${MEMORY_TOTAL}MiB"
+	echo "\${color2}Temperature\$color ${TEMPERATURE}°C\${alignr}\${color2}Utilization\$color \${alignr}${UTILIZATION}%"
+	echo "\${color2}Memory\$color\${alignr}${MEMORY_USED} + ${MEMORY_FREE} = ${MEMORY_TOTAL}MiB"
+    fi
 else
-    echo OFF > "${OUTPUT_FILE}"
+    echo OFF
 fi
